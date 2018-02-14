89a2a5f6135c577ac8b7f65e9cdae254dbde49fd
esteban <esteban_armendariz@hotmail.com>
2018-02-13 20:24:15 -0600

    set up node and gulp also packages and less for css

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+"use strict"; 
+
+// imports
+const gulp = require ('gulp')
+const htmlbeautify = require('gulp-html-beautify');
+const less = require('gulp-less');
+const path = require('path');
+
+// paths
+const htmlSources = './*.html'
+const cssSources = './css/*.less'
+
+  
+ gulp.task('less', function () {
+   return gulp.src('./css/*.less')
+     .pipe(less())
+     .pipe(gulp.dest('./css'));
+ });
+
+
+
+
+gulp.task('htmlbeautify', function() {
+gulp.src(htmlSources)
+    .pipe(htmlbeautify({indentSize: 2}))
+    .pipe(gulp.dest('./'))
+});
+
+gulp.task ('default' , ['htmlbeautify', 'less']); 
