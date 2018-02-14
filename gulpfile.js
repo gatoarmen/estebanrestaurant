2dfbb187ff1dc4978312179f51c31410e5f845ec
esteban <esteban_armendariz@hotmail.com>
2018-02-13 21:49:18 -0600

    added 3 new task to the json

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@
 const gulp = require ('gulp')
 const htmlbeautify = require('gulp-html-beautify');
 const less = require('gulp-less');
-const path = require('path');
+const autoprefixer = require('gulp-autoprefixer');
+const bs = require('browser-sync').create();
 
 // paths
 const htmlSources = './*.html'
@@ -13,9 +14,23 @@ const cssSources = './css/*.less'
   
  gulp.task('less', function () {
    return gulp.src('./css/*.less')
-     .pipe(less())
+     .pipe(less()
+     .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+    })))
+     
      .pipe(gulp.dest('./css'));
  });
+
+
+ gulp.task('browser-sync', function() {
+    bs.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
 
 
 
@@ -27,3 +42,12 @@ gulp.src(htmlSources)
 });
 
 gulp.task ('default' , ['htmlbeautify', 'less']); 
+
+
+gulp.task('watch', function(){
+    gulp.watch(cssSources, ['less']).on('change', bs.reload);
+    gulp.watch(htmlSources, ['htmlbeautify']).on('change', bs.reload);
+   })
+    
+   gulp.task('default', ['browser-sync', 'watch']);
+ 
